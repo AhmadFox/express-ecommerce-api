997baeaf92636b3729e06ebf86a16bf5caafc441
Ahmad Issa Gharaibeh <GHARAIBEH.AHMAD88@GMAIL.COM>
2023-12-06 16:39:07 +0300

    create .env file

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({path:'config.env'});
 
 const app = express();
 
+
+const mood = process.env.NODE_ENV;
+if ( mood === 'development' ) {
+	app.use(morgan('dev'));
+	console.log(`mode: ${mood}`);
+}
+
 app.get('/', (req, res) => {
-	res.send('our app v1');
+	res.send('our app v3');
 })
 
-app.listen(8000, () => {
-	console.log('App Running');
+const port = process.PORT || 8000;
+app.listen(port, () => {
+	console.log(`App Running on port ${port}`);
 });
